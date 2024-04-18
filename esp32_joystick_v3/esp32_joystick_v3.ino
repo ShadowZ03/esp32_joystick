e37c7dffe3466c9adafd26ee1f995b5b8b3b62b9
ShadowZ03 <dmitrigatto@gmail.com>
2024-04-18 16:01:57 -0400

    adding in webhook from other project

--- esp32_joystick_v3/esp32_joystick_v3.ino
+++ esp32_joystick_v3/esp32_joystick_v3.ino
@@ -38,27 +38,78 @@ void setup() {
         file.close();
     });
 
-    server.on("/joystick", HTTP_POST, []() {
-        StaticJsonDocument<200> doc;
-        deserializeJson(doc, server.arg("plain"));
-
-        int x = doc["x"];
-        int y = doc["y"];
-
-        // Map joystick data to PWM values for motors
-        int motor1Speed = map(x, 0, 1023, 0, 255);
-        int motor2Speed = map(y, 0, 1023, 0, 255);
-
-        // Use motor1Speed and motor2Speed to control motors with PWM
-        // Code to control motors with PWM
-        Serial.printf("Motor 1 Speed: %d, Motor 2 Speed: %d\n", motor1Speed, motor2Speed);
-
-        server.send(200, "text/plain", "OK");
-    });
+//    server.on("/joystick", HTTP_POST, []() {
+//        StaticJsonDocument<200> doc;
+//        deserializeJson(doc, server.arg("plain"));
+//
+//        int x = doc["x"];
+//        int y = doc["y"];
+//
+//        // Map joystick data to PWM values for motors
+//        int motor1Speed = map(x, 0, 1023, 0, 255);
+//        int motor2Speed = map(y, 0, 1023, 0, 255);
+//
+//        // Use motor1Speed and motor2Speed to control motors with PWM
+//        // Code to control motors with PWM
+//        Serial.printf("Motor 1 Speed: %d, Motor 2 Speed: %d\n", motor1Speed, motor2Speed);
+//
+//        server.send(200, "text/plain", "OK");
+//    });
 
     server.begin();
+    webSocket.begin();                                  // start websocket
+    webSocket.onEvent(webSocketEvent);                  // define a callback function -> what does the ESP32 need to do when an event from the websocket is received? -> run function "webSocketEvent()"
 }
 
 void loop() {
     server.handleClient();
+      webSocket.loop();                                   // Update function for the webSockets 
+  
+  unsigned long now = millis();                       // read out the current "time" ("millis()" gives the time in ms since the Arduino started)
+  if ((unsigned long)(now - previousMillis) > interval) { // check if "interval" ms has passed since last time the clients were updated
+    
+    String jsonString = "";                           // create a JSON string for sending data to the client
+    StaticJsonDocument<200> doc;                      // create a JSON container
+    JsonObject object = doc.to<JsonObject>();         // create a JSON Object
+    object["rand1"] = random(100);                    // write data into the JSON object -> I used "rand1" and "rand2" here, but you can use anything else
+    object["rand2"] = random(100);
+    serializeJson(doc, jsonString);                   // convert JSON object to string
+    Serial.println(jsonString);                       // print JSON string to console for debug purposes (you can comment this out)
+    webSocket.broadcastTXT(jsonString);               // send JSON string to clients
+    
+    previousMillis = now;                             // reset previousMillis
+  }
+  void webSocketEvent(byte num, WStype_t type, uint8_t * payload, size_t length) {      // the parameters of this callback function are always the same -> num: id of the client who send the event, type: type of message, payload: actual data sent and length: length of payload
+  switch (type) {                                     // switch on the type of information sent
+    case WStype_DISCONNECTED:                         // if a client is disconnected, then type == WStype_DISCONNECTED
+      Serial.println("Client " + String(num) + " disconnected");
+      break;
+    case WStype_CONNECTED:                            // if a client is connected, then type == WStype_CONNECTED
+      Serial.println("Client " + String(num) + " connected");
+      // optionally you can add code here what to do when connected
+      break;
+    case WStype_TEXT:                                 // if a client has sent data, then type == WStype_TEXT
+      // try to decipher the JSON string received
+      StaticJsonDocument<200> doc;                    // create a JSON container
+      DeserializationError error = deserializeJson(doc, payload);
+      if (error) {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.f_str());
+        return;
+      }
+      else {
+        // JSON string was received correctly, so information can be retrieved:
+        const char* g_brand = doc["brand"];
+        const char* g_type = doc["type"];
+        const int g_year = doc["year"];
+        const char* g_color = doc["color"];
+        Serial.println("Received guitar info from user: " + String(num));
+        Serial.println("Brand: " + String(g_brand));
+        Serial.println("Type: " + String(g_type));
+        Serial.println("Year: " + String(g_year));
+        Serial.println("Color: " + String(g_color));
+      }
+      Serial.println("");
+      break;
+  }  
 }
