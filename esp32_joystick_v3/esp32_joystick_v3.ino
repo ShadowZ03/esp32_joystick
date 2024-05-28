548f6b91089e48e541ccf53f59ff525512b9bf6f
ShadowZ03 <98532884+ShadowZ03@users.noreply.github.com>
2024-05-28 04:18:24 -0400

    Update esp32_joystick_v3.ino
    Updated code to work with LEDs

--- esp32_joystick_v3/esp32_joystick_v3.ino
+++ esp32_joystick_v3/esp32_joystick_v3.ino
@@ -1,115 +1,98 @@
 #include <WiFi.h>
 #include <WebServer.h>
-#include <WebSocketsServer.h>                         // needed for instant communication between client and server through Websockets
+#include <WebSocketsServer.h>
 #include <ArduinoJson.h>
 #include <SPIFFS.h>
 
-
+// SSID and password of Wifi connection:
 const char* ssid = "YourSSID";
 const char* password = "YourPassword";
 
-int interval = 1000;                                  // send data to the client every 1000ms -> 1s
-unsigned long previousMillis = 0;                     // we use the "millis()" command for time reference and this will output an unsigned long
+// Define LED pins
+const int ledPinN = 13;
+const int ledPinE = 14;
+const int ledPinS = 15;
+const int ledPinW = 16;
 
 // Initialization of webserver and websocket
-WebServer server(80);                                 // the server uses port 80 (standard port for websites
-WebSocketsServer webSocket = WebSocketsServer(81);    // the websocket uses port 81 (standard port for websockets
+WebServer server(80);                                 
+WebSocketsServer webSocket = WebSocketsServer(81);    
 
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);                              
+  pinMode(ledPinN, OUTPUT);
+  pinMode(ledPinE, OUTPUT);
+  pinMode(ledPinS, OUTPUT);
+  pinMode(ledPinW, OUTPUT);
 
-    WiFi.softAP(ssid, password);
-    Serial.println("Access Point Started");
-    Serial.println(WiFi.softAPIP());
+  WiFi.softAP(ssid, password);
+  Serial.println("Access Point Started");
+  Serial.println(WiFi.softAPIP());
 
-    if (!SPIFFS.begin(true)) {
+  if (!SPIFFS.begin(true)) {
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
-    }
+  }
   
-    server.on("/", HTTP_GET, []() {
-        File file = SPIFFS.open("/index", "r");
-        if (!file) {
-            Serial.println("Failed to open file");
-            server.send(404, "text/plain", "File not found");
-            return;
-        }
-        server.streamFile(file, "text/html");
-        file.close();
-    });
-
-//    server.on("/joystick", HTTP_POST, []() {
-//        StaticJsonDocument<200> doc;
-//        deserializeJson(doc, server.arg("plain"));
-//
-//        int x = doc["x"];
-//        int y = doc["y"];
-//
-//        // Map joystick data to PWM values for motors
-//        int motor1Speed = map(x, 0, 1023, 0, 255);
-//        int motor2Speed = map(y, 0, 1023, 0, 255);
-//
-//        // Use motor1Speed and motor2Speed to control motors with PWM
-//        // Code to control motors with PWM
-//        Serial.printf("Motor 1 Speed: %d, Motor 2 Speed: %d\n", motor1Speed, motor2Speed);
-//
-//        server.send(200, "text/plain", "OK");
-//    });
-
-    server.begin();
-    webSocket.begin();                                  // start websocket
-    webSocket.onEvent(webSocketEvent);                  // define a callback function -> what does the ESP32 need to do when an event from the websocket is received? -> run function "webSocketEvent()"
+  server.on("/", HTTP_GET, []() {                              
+    File file = SPIFFS.open("/index.html", "r");
+    server.streamFile(file, "text/html");
+    file.close();
+  });
+  server.begin();                                     
+  webSocket.begin();                                  
+  webSocket.onEvent(webSocketEvent);                  
 }
 
 void loop() {
-    server.handleClient();
-      webSocket.loop();                                   // Update function for the webSockets 
-  
-  unsigned long now = millis();                       // read out the current "time" ("millis()" gives the time in ms since the Arduino started)
-  if ((unsigned long)(now - previousMillis) > interval) { // check if "interval" ms has passed since last time the clients were updated
-    
-    String jsonString = "";                           // create a JSON string for sending data to the client
-    StaticJsonDocument<200> doc;                      // create a JSON container
-    JsonObject object = doc.to<JsonObject>();         // create a JSON Object
-    object["rand1"] = random(100);                    // write data into the JSON object -> I used "rand1" and "rand2" here, but you can use anything else
-    object["rand2"] = random(100);
-    serializeJson(doc, jsonString);                   // convert JSON object to string
-    Serial.println(jsonString);                       // print JSON string to console for debug purposes (you can comment this out)
-    webSocket.broadcastTXT(jsonString);               // send JSON string to clients
-    
-    previousMillis = now;                             // reset previousMillis
-  }
-  void webSocketEvent(byte num, WStype_t type, uint8_t * payload, size_t length) {      // the parameters of this callback function are always the same -> num: id of the client who send the event, type: type of message, payload: actual data sent and length: length of payload
-  switch (type) {                                     // switch on the type of information sent
-    case WStype_DISCONNECTED:                         // if a client is disconnected, then type == WStype_DISCONNECTED
+  server.handleClient();                              
+  webSocket.loop();                                   
+}
+
+void webSocketEvent(byte num, WStype_t type, uint8_t * payload, size_t length) {
+  switch (type) {
+    case WStype_DISCONNECTED:
       Serial.println("Client " + String(num) + " disconnected");
       break;
-    case WStype_CONNECTED:                            // if a client is connected, then type == WStype_CONNECTED
+    case WStype_CONNECTED:
       Serial.println("Client " + String(num) + " connected");
-      // optionally you can add code here what to do when connected
       break;
-    case WStype_TEXT:                                 // if a client has sent data, then type == WStype_TEXT
-      // try to decipher the JSON string received
-      StaticJsonDocument<200> doc;                    // create a JSON container
+    case WStype_TEXT:
+      StaticJsonDocument<200> doc;
       DeserializationError error = deserializeJson(doc, payload);
       if (error) {
         Serial.print(F("deserializeJson() failed: "));
         Serial.println(error.f_str());
         return;
+      } else {
+        int g_x = doc["x"];
+        int g_y = doc["y"];
+        Serial.println("Received action from: " + String(num));
+        Serial.println("x: " + String(g_x));
+        Serial.println("y: " + String(g_y));
+
+        // Determine LED states based on joystick input
+        bool north = g_y > 100;
+        bool south = g_y < -100;
+        bool east = g_x > 100;
+        bool west = g_x < -100;
+
+        // Set LED states
+        digitalWrite(ledPinN, north ? HIGH : LOW);
+        digitalWrite(ledPinS, south ? HIGH : LOW);
+        digitalWrite(ledPinE, east ? HIGH : LOW);
+        digitalWrite(ledPinW, west ? HIGH : LOW);
+
+        // For debugging
+        Serial.println("LED States:");
+        Serial.println("North: " + String(north));
+        Serial.println("South: " + String(south));
+        Serial.println("East: " + String(east));
+        Serial.println("West: " + String(west));
+
+        serializeJsonPretty(doc, Serial);
+        Serial.println();
       }
-      else {
-        // JSON string was received correctly, so information can be retrieved:
-        const char* g_brand = doc["brand"];
-        const char* g_type = doc["type"];
-        const int g_year = doc["year"];
-        const char* g_color = doc["color"];
-        Serial.println("Received guitar info from user: " + String(num));
-        Serial.println("Brand: " + String(g_brand));
-        Serial.println("Type: " + String(g_type));
-        Serial.println("Year: " + String(g_year));
-        Serial.println("Color: " + String(g_color));
-      }
-      Serial.println("");
       break;
-  }  
+  }
 }
